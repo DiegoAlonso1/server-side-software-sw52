b8b5ff2c7d98f813a3af03a6db4288223ba9f372
DiegoAlonso1 <diegoalonsooliverab@gmail.com>
2021-06-02 20:22:58 -0500

    feat: Project Created

---
+++ UltimateTeamApi/Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimateTeamApi.Persistance.Repositories
+{
+    public class UserRepository
+    {
+    }
+}
