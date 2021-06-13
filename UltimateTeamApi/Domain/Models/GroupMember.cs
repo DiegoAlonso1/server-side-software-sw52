37a2befd14d974af61708bb4637840208c2fa522
Andreluis Ingaroca Torres <andreluispit@gmail.com>
2021-06-13 00:49:44 -0500

    feat: GroupMember

--- UltimateTeamApi/Domain/Models/GroupMember.cs
+++ UltimateTeamApi/Domain/Models/GroupMember.cs
@@ -7,5 +7,10 @@ namespace UltimateTeamApi.Domain.Models
 {
     public class GroupMember
     {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public bool UserCreator { get; set; }
     }
 }
