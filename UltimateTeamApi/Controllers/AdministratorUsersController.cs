c41b1572fcdb79d6a697ebf657a12150a19f4994
George <sanchez28112001@gmail.com>
2021-06-15 01:04:29 -0500

    feat: finish admin and all payments controllers

---
+++ UltimateTeamApi/Controllers/AdministratorUsersController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateTeamApi.Domain.Models;
+using UltimateTeamApi.Domain.Services;
+using UltimateTeamApi.Resources;
+
+namespace UltimateTeamApi.Controllers
+{
+    [Route("api/administrators/{administratorId}/users")]
+    [Produces("application/json")]
+    [ApiController]
+    public class AdministratorUsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public AdministratorUsersController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        /******************************************/
+            /*GET ALL USERS BY ADMIN ID ASYNC*/
+        /******************************************/
+
+        [SwaggerOperation(
+            Summary = "Get All Users By Administrator Id",
+            Description = "Get List of All Users By Administrator Id",
+            OperationId = "GetAllUsersByAdministratorId")]
+        [SwaggerResponse(200, "List of Users By Administrator Id", typeof(IEnumerable<UserResource>))]
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<UserResource>), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IEnumerable<UserResource>> GetAllByAdministratorIdAsync(int administratorId)
+        {
+            var users = await _userService.GetAllByAdministratorIdAsync(administratorId);
+            var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+            return resources;
+        }
+
+
+    }
+
+}
---
+++ UltimateTeamApi/Controllers/SubscriptionTypeSubscriptionBillsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateTeamApi.Domain.Models;
+using UltimateTeamApi.Domain.Services;
+using UltimateTeamApi.Resources;
+
+namespace UltimateTeamApi.Controllers
+{
+    [Route("api/sub-types/{subTypeId}/subscriptionBills")]
+    [Produces("application/json")]
+    [ApiController]
+    public class SubscriptionTypeSubscriptionBillsController : ControllerBase
+    {
+        private readonly ISubscriptionBillService _subscriptionBillService;
+        private readonly IMapper _mapper;
+
+        public SubscriptionTypeSubscriptionBillsController(ISubscriptionBillService subscriptionBillService, IMapper mapper)
+        {
+            _subscriptionBillService = subscriptionBillService;
+            _mapper = mapper;
+        }
+
+        /******************************************/
+            /*GET ALL SUB-BILLS BY TYPE ID ASYNC*/
+        /******************************************/
+
+        [SwaggerOperation(
+            Summary = "Get All Sub Bills By Type Id",
+            Description = "Get List of Sub Bills By Type Id",
+            OperationId = "GetAllSubBillsByTypeId")]
+        [SwaggerResponse(200, "List of Sub Bills By Type Id", typeof(IEnumerable<SubscriptionBillResource>))]
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<UserResource>), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IEnumerable<SubscriptionBillResource>> GetAllByAdministratorIdAsync(int subTypeId)
+        {
+            var subscriptionBills = await _subscriptionBillService.GetAllBySubscriptionTypeIdAsync(subTypeId);
+            var resources = _mapper.Map<IEnumerable<SubscriptionBill>, IEnumerable<SubscriptionBillResource>>(subscriptionBills);
+            return resources;
+        }
+    }
+}
