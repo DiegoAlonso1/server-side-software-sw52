8cac462c53ed74f691f497bcfd6911fcdd022966
George <sanchez28112001@gmail.com>
2021-06-14 20:22:09 -0500

    feat: add Administrator model

--- UltimateTeamApi/Domain/Models/Administrator.cs
+++ UltimateTeamApi/Domain/Models/Administrator.cs
@@ -8,5 +8,11 @@ namespace UltimateTeamApi.Domain.Models
     public class Administrator
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Area { get; set; }
+
+        public List<User> Users { get; set; }
+
     }
 }
