51000b8f92b394b6db96e9512af9406751880fa1
Sebastian <sebastiansasaki@gmail.com>
2021-06-15 12:09:27 -0500

    test: Friendship and Notification SpecFlowTest addded

---
+++ UltimateTeamApi.SpecFlowTest/Friendship/FriendshipSteps.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using UltimateTeamApi.Domain.Models;
+
+namespace UltimateTeamApi.SpecFlowTest.Friendship
+{
+    [Binding]
+    class FriendshipSteps : BaseTest
+    {
+        private string FriendshipEndpoint { get; set; }
+
+        public FriendshipSteps()
+        {
+            FriendshipEndpoint = $"{ApiUri}api/friendships";
+        }
+    }
+}
---
+++ UltimateTeamApi.SpecFlowTest/Notification/NotificationSteps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateTeamApi.SpecFlowTest.Notification
+{
+    class NotificationSteps : BaseTest
+    {
+        private string NotificationEndpoint { get; set; }
+
+        public NotificationSteps()
+        {
+            NotificationEndpoint = $"{ApiUri}api/notifications";
+        }
+    }
+}
