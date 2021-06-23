7ec118350cb4cf55a46393b461537cadb4aee88f
DiegoHZ5 <safairus@gmail.com>
2021-06-22 22:42:32 -0500

    feat: miroResource added

---
+++ UltimateTeamApi.ExternalTools/Resources/MiroUserConnectionResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateTeamApi.ExternalTools.Resources
+{
+    public class MiroUserConnectionResource
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public MiroUserResource User { get; set; }
+        public static DateTime CreatedAt { get; }
+        public DateTime ModifiedAt { get; set; }
+        public MiroUserResource CreatedBy { get; set; }
+        public MiroUserResource ModifiedBy { get; set; }
+    }
+}
---
+++ UltimateTeamApi.ExternalTools/Resources/MiroUserResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateTeamApi.ExternalTools.Resources
+{
+    public class MiroUserResource
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public static DateTime CreatedAt { get; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Role { get; set; }
+        public string Industry { get; set; }
+        public string Email { get; set; }
+        public string State{ get; set; }
+    }
+}
