4541027fdfb3676f7f8fc97d6e3927fb5b0b2e47
George <sanchez28112001@gmail.com>
2021-06-30 11:06:24 -0500

    feat: add Pay Pal Controller

--- UltimateTeamApi.ExternalTools/Services/PayPalService.cs
+++ UltimateTeamApi.ExternalTools/Services/PayPalService.cs
@@ -14,7 +14,7 @@ namespace UltimateTeamApi.ExternalTools.Services
 { 
     public class PayPalService : IPayPalService
     {
-        public  async Task<PayPalTokenResponse> GetToken()
+        public async Task<PayPalTokenResponse> GetToken()
         {
             var httpClient = new HttpClient();
             var request = new HttpRequestMessage(new HttpMethod("POST"), "https://api-m.sandbox.paypal.com/v1/oauth2/token");
@@ -61,8 +61,8 @@ namespace UltimateTeamApi.ExternalTools.Services
             request.Headers.TryAddWithoutValidation("Prefer", "return=representation");
 
             string dataString = " \n      \"plan_id\": \""+
-                resource.PlanId
-                +"\",\n      \"start_time\": \""+GetDate()
+                $"{resource.PlanId}"
+                +"\",\n      \"start_time\": \""+GetDate().ToString()
                 +"\",\n      \"application_context\": {\n        \"brand_name\": \"example company\",\n        \"locale\": \"en-US\",\n        \"shipping_preference\": \"SET_PROVIDED_ADDRESS\",\n        \"user_action\": \"SUBSCRIBE_NOW\",\n        \"payment_method\": {\n          \"payer_selected\": \"PAYPAL\",\n          \"payee_preferred\": \"IMMEDIATE_PAYMENT_REQUIRED\"\n        },\n        \"return_url\": \"https://example.com/returnUrl\",\n        \"cancel_url\": \"https://example.com/cancelUrl\"\n      }\n ";
 
             request.Content = new StringContent(dataString);
---
+++ UltimateTeamApi/Controllers/PayPalSubscriptionsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateTeamApi.ExternalTools.Domain.Services;
+using UltimateTeamApi.ExternalTools.Resources;
+
+namespace UltimateTeamApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class PayPalSubscriptionsController : ControllerBase
+    {
+        private readonly IPayPalService _payPalService;
+        private readonly IMapper _mapper;
+
+        public PayPalSubscriptionsController(IPayPalService payPalService, IMapper mapper)
+        {
+            _payPalService = payPalService;
+            _mapper = mapper;
+        }
+
+        /******************************************/
+                /*POST PAY PAL SUBSCRIPTION*/
+        /******************************************/
+        [SwaggerOperation(
+            Summary = "Create a Pay Pal Subscription",
+            Description = "Create a Pay Pal Subscription",
+            OperationId = "Post Pay Pal Subscription")]
+        [SwaggerResponse(200, "Subscription Pay Pal", typeof(PayPalSubscriptionResource))]
+
+        [HttpPost]
+        [ProducesResponseType(typeof(PayPalSubscriptionResource), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 404)]
+        public async Task<IActionResult> GetByIdAsync([FromBody] SaveSuscription resource)
+        {
+            var result = await _payPalService.SuscribeToAPlan(resource);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            return Ok(result.Resource);
+        }
+    }
+}
